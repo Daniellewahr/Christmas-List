dffd3303d93faa9156d2283949e57d3e21edc4a0
Danielle Wahrhaftig <daniellewahrhaftig@gmail.com>
2022-11-28 18:02:55 -0500

    Adding working list

---
+++ background.js
@@ -0,0 +1,11 @@
+
+
+let colour = '#3aa757';
+
+chrome.runtime.onInstalled.addListener(() => {
+
+chrome.storage.sync.set({ colour });
+
+console.log('Default background colour set to %cgreen', `colour: ${colour}`);
+
+});
